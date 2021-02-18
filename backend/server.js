cad3fc1ed9ef07cfa5a37629140ab5556b492977
Dan <dan.bayford@gmail.com>
2021-02-18 16:49:22 +0000

    feat: set up simple server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (_, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
